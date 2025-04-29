c2c90142eb95874029b758bec498a401d379188a
Noor2003-hub <sun.n00r.n@gmail.com>
2025-04-29 14:23:41 +0300

    protected authenticated pages

---
+++ lms/src/app/(frontend)/(authenticated)/layout.tsx
@@ -0,0 +1,16 @@
+import {redirect} from'next/navigation'
+import React,{Children, ReactNode} from 'react'
+import {getUser} from './actions/getUsers';
+interface LayoutProps{
+    children:ReactNode
+}
+const Layout: FC<LayoutProps>=async ({children})=>{
+    const user=await getUser();
+    if (!user){
+        redirect('/login')
+        return null
+    }
+    return <>{children}</>
+}
+
+export default Layout;
